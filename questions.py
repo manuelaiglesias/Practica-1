5e3db92f2c6d61034b6ce6cda11854ff98a35776
manuelaiglesias <manuelaiglesiasdelgado@gmail.com>
2026-03-19 12:01:33 -0300

    Modificacion de puntos e impresion de los mismos

--- questions.py
+++ questions.py
@@ -13,6 +13,9 @@ words = [
 word = random.choice(words)
 guessed = []
 attempts = 6
+# Modificacion 2
+puntos = 0 
+#
 
 print("¡Bienvenido al Ahorcado!")
 print()
@@ -30,6 +33,9 @@ while attempts > 0:
 # Verificar si el jugador ya adivinó la palabra completa
      if "_" not in progress:
          print("¡Ganaste!")
+# Modificacion 2 
+         puntos +=6
+#                  
          break
      print(f"Intentos restantes: {attempts}")
      print(f"Letras usadas: {', '.join(guessed)}")
@@ -49,6 +55,12 @@ while attempts > 0:
          guessed.append(letter)
          attempts -= 1
          print("Esa letra no está en la palabra.")
+ # Modificacion 2
+         puntos -=1
+#                 
      print()
 else:
      print(f"¡Perdiste! La palabra era: {word}")
+# Modificacion 2
+     puntos = 0
+print(f"El puntaje es {puntos}")     
